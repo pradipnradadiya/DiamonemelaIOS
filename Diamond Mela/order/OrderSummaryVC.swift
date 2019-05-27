9c1d1ddd7b27023968874a2289510c38547cd90c
mac <mac@macs-iMac-Pro.local>
2019-05-27 13:29:02 +0530

    27-05 COMMIT

---
+++ Diamond Mela/order/OrderSummaryVC.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class OrderSummaryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
